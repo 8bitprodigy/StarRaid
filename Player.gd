a7a4602493173eb7325c51b0329d9b37a4e79239
Luke Wilson <asmoaesl@gmail.com>
2019-04-07 01:44:58 -0500

    Switch to GLES3 and project config

--- Player.gd
+++ Player.gd
@@ -1,6 +1,6 @@
 extends Spatial
 
-var speed = 50 # Pretty fast -- this is about 100 mph
+var speed = 20 # Pretty fast -- this is about 100 mph
 var pitch_add = 1
 var roll_add = 1
 var yaw_add = 0
@@ -18,6 +18,9 @@ func _process(delta):
 	if Input.is_action_pressed("pitch_down"):
 		rotate_object_local(Vector3(1,0,0), -pitch_add * delta)
 	if Input.is_action_pressed("roll_right"):
-		rotate_object_local(Vector3(0,0,1), -pitch_add * delta)
+		rotate_object_local(Vector3(0,0,1), -roll_add * delta)
 	if Input.is_action_pressed("roll_left"):
-		rotate_object_local(Vector3(0,0,1), pitch_add * delta)
+		rotate_object_local(Vector3(0,0,1), roll_add * delta)
+	
+	# Move
+	translate_object_local(Vector3(0, 0, -speed * delta))
