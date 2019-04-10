d49b3a78c2ed09f7a69d209fdeb1c2a362f383d9
Luke Wilson <asmoaesl@gmail.com>
2019-04-09 23:37:56 -0500

    Make game pixelated

--- scenes/HUD.gd
+++ scenes/HUD.gd
@@ -11,6 +11,7 @@ func _draw():
 	
 	# Reticle
 	draw_x(Vector2(d.x / 2 - 5, d.y / 2 - 5 + 40), Vector2(d.x / 2 + 5, d.y / 2 + 5 + 40))
+	# Reticle needs to be replaced with the standard unfilled circle and three lines out of it: left, top, right
 	
 	# Roll gyroscope
 	var top = Vector2(0, -50)
@@ -18,8 +19,8 @@ func _draw():
 	var roll_rot = $"../../".rotation.z
 	top = top.rotated(roll_rot)
 	bottom = bottom.rotated(roll_rot)
-	draw_line(Vector2(top.x + (d.x/2), top.y + 50 + (180-100)/2), Vector2(bottom.x + (d.x/2), bottom.y + 50 + (180-100)/2), Color.green, 2)
+	draw_line(Vector2(top.x + (d.x/2), top.y + 50 + (180-100)/2), Vector2(bottom.x + (d.x/2), bottom.y + 50 + (180-100)/2), Color.green, 3)
 
 func draw_x(tl: Vector2, br: Vector2):
-	draw_line(tl, br, Color.green, 2)
-	draw_line(Vector2(tl.x, br.y), Vector2(br.x, tl.y), Color.green, 2)
+	draw_line(tl, br, Color.green, 3)
+	draw_line(Vector2(tl.x, br.y), Vector2(br.x, tl.y), Color.green, 3)
