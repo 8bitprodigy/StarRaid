6208285dfc5f50a9dbd067f7ace0fed3b3bb9502
Luke Wilson <asmoaesl@gmail.com>
2019-04-09 23:19:45 -0500

    Fixed roll HUD gyro

--- scenes/HUD.gd
+++ scenes/HUD.gd
@@ -13,12 +13,12 @@ func _draw():
 	draw_x(Vector2(d.x / 2 - 5, d.y / 2 - 5 + 40), Vector2(d.x / 2 + 5, d.y / 2 + 5 + 40))
 	
 	# Roll gyroscope
-	var top = Vector2(d.x / 2, d.y / 3)
-	var bottom = Vector2(d.x / 2, d.y * (2 / 3))
+	var top = Vector2(0, -50)
+	var bottom = Vector2(0, 50)
 	var roll_rot = $"../../".rotation.z
 	top = top.rotated(roll_rot)
 	bottom = bottom.rotated(roll_rot)
-	draw_line(top, bottom, Color.green, 3)
+	draw_line(Vector2(top.x + (d.x/2), top.y + 50 + (180-100)/2), Vector2(bottom.x + (d.x/2), bottom.y + 50 + (180-100)/2), Color.green, 2)
 
 func draw_x(tl: Vector2, br: Vector2):
 	draw_line(tl, br, Color.green, 2)
