c06806b6f098ae962aebfbb1a7d69473e161b410
Luke Wilson <asmoaesl@gmail.com>
2019-04-13 15:09:39 -0500

    Simple ricochet

--- scripts/Bullet.gd
+++ scripts/Bullet.gd
@@ -20,4 +20,8 @@ func _physics_process(delta):
 		# send "hit by this bullet" message to receiver
 		# produce effect
 		# consider ricocheting
+		if rad2deg(velocity.angle_to(collision.normal)) < 100:
+			velocity = velocity.bounce(collision.normal) / 2
+			# update rotation
+			# look_at(velocity, Vector3(0, 1, 0)) TODO: make this work
 		print("Bullet traveled")
